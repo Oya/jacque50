a1b495df9a618cdb2cdca0fa0932c84a1f33a4d2
Oya <marciooya@gmail.com>
2015-11-17 23:49:04 -0200

    Gmail

--- js/custom.js
+++ js/custom.js
@@ -581,7 +581,7 @@
 		            $(form).ajaxSubmit({
 		                type:"POST",
 		                data: $(form).serialize(),
-		                url:"http://formspree.io/jaclucas@yahoo.com.br",
+		                url:"http://formspree.io/50dajacque@gmail.com",
 		                success: function() {
 		                    $('#contactform').clearForm();
 		                    $('#cf-success').css("display", "block").css("opacity", "1");
